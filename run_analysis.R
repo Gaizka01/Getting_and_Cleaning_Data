bcfbaa256f071f57f44cd28caad88b51ff7c7c96
Gaizka01 <ggaretxana@gmail.com>
2014-05-23 03:36:24 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,18 @@
 #course project data reading    
 library (stringr)
-#Generando nombres de variables, sólo mean() y std()
+#Generando nombres de variables, sï¿½lo mean() y std()
 varnames = read.table("features.txt", sep ="")
 x = str_split_fixed(varnames$V2, pattern = "-", n = 3)
 y = paste(x[,1], x[,2], sep = " ")
 m = x[x[,2]=="mean()" | x[,2]=="std()",]
 col = paste(m[,1], m[,2])
-#fusionando los datos del grupo test y train, sólo mean() y std()
+#fusionando los datos del grupo test y train, sï¿½lo mean() y std()
 xtest = read.table("X_test.txt")
 xtrain = read.table("X_train.txt")
 vec = which(x[,2]=="mean()" | x[,2]=="std()")
 xmeansd = rbind (xtest[,vec], xtrain[,vec])
 colnames(xmeansd) = col
-#fusión de columnas
+#fusiï¿½n de columnas
 fusion = t(apply(xmeansd, 1, function(x) tapply(x, colnames(xmeansd), mean)))
 #identificando a los individuos
 xsubj = read.table("subject_test.txt")
@@ -21,8 +21,8 @@ individuos = rbind(xsubj, ysubj)
 #nombres adecuados a las actividades
 ytrain = read.table("y_train.txt")
 ytest = read.table("y_test.txt")
-ytrain[1,1] = 5 #para corregir caracteres extraños
-ytest[1,1] = 5 #para corregir caracteres extraños
+ytrain[1,1] = 5 #para corregir caracteres extraï¿½os
+ytest[1,1] = 5 #para corregir caracteres extraï¿½os
 activity = rbind(ytest, ytrain)
 act = read.table("activity_labels.txt", sep = " ")
 actname = function(act, activity){
@@ -54,5 +54,5 @@ actividades = actname(act, activity)
 completo =cbind (fusion, individuos, actividades)
 #agregando por filas y data final
 fila = paste(completo[, 35], completo[, 36])
-res = apply(completo[,1:34], 2, function(x) tapply(x, fila, mean))
-write.table(res, file = "tidydata.txt")
+tidydata = apply(completo[,1:34], 2, function(x) tapply(x, fila, mean))
+write.table(tidydata, file = "tidydata.txt")
